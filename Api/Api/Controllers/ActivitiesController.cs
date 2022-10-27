a603dbad1ee87b5645365395b7e511bddb55c553
Ali Khorsand <ali.khorsand@live.com>
2022-10-27 13:39:20 +0330

    Initial Api Commit

---
+++ Api/Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Api.Controllers;
+
+public class ActivitiesController: BaseApiController
+{
+    private readonly DataContext _dataContext;
+
+    public ActivitiesController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        var activities = await _dataContext.Activities.ToListAsync();
+
+        return activities;
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        var activity = await _dataContext.Activities.FindAsync(id);
+
+        return activity;
+    }
+}
